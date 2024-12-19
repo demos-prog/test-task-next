97e023361b6d037bbd8286f0d45a19c361783828
demos-prog <demosne@gmail.com>
2024-12-19 11:25:12 +0300

    fix: api routing

---
+++ app/api/read/route.ts
@@ -0,0 +1,17 @@
+import type { NextRequest } from "next/server";
+import { NextResponse } from "next/server";
+import db from "@/db/connection";
+
+export async function GET(request: NextRequest) {
+  try {
+    const collection = db.collection("posts");
+    const results = await collection.find({}).toArray();
+    return NextResponse.json(results);
+  } catch (err) {
+    console.error(err);
+    return NextResponse.json(
+      { error: "Internal Server Error" },
+      { status: 500 }
+    );
+  }
+}
